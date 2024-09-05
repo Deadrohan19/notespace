99713646a9c5130a8f87d12a2004f6354db74340
rohan <rohanjaiswal511@gmail.com>
2024-09-05 08:13:02 +0530

    init project

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <Button variant="default" size="lg" className="m-4 font-bold">Click me</Button>
+  );
+}
