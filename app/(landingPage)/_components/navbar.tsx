bbb44ab98e5dd1a36b4341881beb9ef65d1a5b75
rohan <rohanjaiswal511@gmail.com>
2024-09-07 18:37:17 +0530

    Add clerk:authentication and convex:backend/database

--- app/(landingPage)/_components/navbar.tsx
+++ app/(landingPage)/_components/navbar.tsx
@@ -1,23 +1,56 @@
-"use client"
+"use client";
 
-import { useScrollTop } from "@/hooks/use-scroll-top"
+import { useScrollTop } from "@/hooks/use-scroll-top";
 import { ModeToggle } from "@/components/mode-toggle";
 import { cn } from "@/lib/utils";
-import Logo from "./logo"
+
+import { useConvexAuth } from "convex/react";
+import { SignInButton, UserButton} from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+import Logo from "./logo";
+import Link from "next/link";
+
 
 const Navbar = () => {
-    const scrolled = useScrollTop();
+  const { isAuthenticated, isLoading } = useConvexAuth();
+  const scrolled = useScrollTop();
   return (
-    <div className={cn(
+    <div
+      className={cn(
         "z-50 bg-[#ffffff] dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full p-6",
         scrolled && "border-b shadow-sm"
-    )}>
+      )}
+    >
       <Logo />
-      <div className="md:ml-auto md:justify-end justify-betweeen w-full flex items-center gap-x-2">
+      <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant={"ghost"} size="sm">
+                Log In
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size="sm">Get Jotion free</Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+            <>
+            <Button variant={"ghost"} size="sm" asChild>
+              <Link href="/documents">
+                Enter Jotion
+              </Link>
+            </Button>
+            <UserButton />
+            </>
+        )}
         <ModeToggle />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ConvexClientProvider } from "@/components/providers/convex-provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -43,6 +44,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ConvexClientProvider>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
@@ -52,6 +54,8 @@ export default function RootLayout({
         >
           {children}
           </ThemeProvider>
+        </ConvexClientProvider>
+
       </body>
     </html>
   );
