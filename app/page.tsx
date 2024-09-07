017c1ab2dc3314e89d3c8f2faf2d4a6d7ef9363e
rohan <rohanjaiswal511@gmail.com>
2024-09-07 12:41:20 +0530

    Add Landing Page

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-export default function Home() {
-  return (
-    <Button variant="default" size="lg" className="m-4 font-bold">Click me</Button>
-  );
-}
