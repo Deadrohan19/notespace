edb4cdd803bea700d26afae0d84d936ade8f44d5
deadrohan19 <rohanjaiswal511@gmail.com>
2024-12-24 00:44:04 +0530

    Changed Auth key in convex

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://regular-dassie-15.clerk.accounts.dev",
+      domain: "https://clerk.notespace.rohanjaiswal.in",
       applicationID: "convex",
     },
   ]
